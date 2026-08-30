2ed73670894b4d9dbe424b34294c8df64d5f5b51
Omkar Hadawale <omkarhadawale00@Gmail.com>
2026-08-30 18:08:00 +0530

    Yo

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// 
-
-
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 10000,    // 10 KB minimum chunk size
+          maxSize: 450000,   // 👈 Force Rolldown to split files if they cross 450 KB
+          groups: [
+            {
+              name: 'vendor-libs',
+              test: /node_modules/,
+              priority: 10
+            }
+          ]
+        }
+      }
+    }
+  }
 })
